1ab0856787317b8b8a8d1483a57b181e79cf48ad
Artin Mohammadi <sheikhartin@gmail.com>
2024-11-02 19:37:18 +0330

    Fix typo

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
 from portfolio.models import Post, Tag
 
